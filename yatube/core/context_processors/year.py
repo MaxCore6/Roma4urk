eeaf9c113f268c1840165bb2b3f80ee94b551166
Max Makarov <maxcore66@gmail.com>
2024-05-23 18:34:34 -0400

    r

--- yatube/core/context_processors/year.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-def year(request):
-    """Добавляет переменную с текущим годом."""
-    current_time = datetime.datetime.now()
-    year = int(current_time.strftime('%Y'))
-    return {
-        'year': year
-    }
